99c70acec8f8f8653aedfd6b968b66e3e6c77ad3
Vinay Verma <vvinay@codeaurora.org>
2018-04-24 17:37:50 +0530

    Add libhardware header library
    Change-Id: I6605dffb63e4564c705ab3f8ee6af5f158550ee4

--- Android.mk
+++ Android.mk
@@ -1,10 +1,10 @@
 ifeq ($(AB_OTA_UPDATER),true)
 LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
-LOCAL_C_INCLUDES += hardware/libhardware/include
 LOCAL_C_INCLUDES += $(TARGET_OUT_HEADERS)/gpt-utils/inc
 LOCAL_CFLAGS += -Wall -Werror
 LOCAL_SHARED_LIBRARIES += liblog librecovery_updater_msm libcutils
+LOCAL_HEADER_LIBRARIES += libhardware_headers
 LOCAL_SRC_FILES := boot_control.cpp
 LOCAL_MODULE_RELATIVE_PATH := hw
 LOCAL_MODULE := bootctrl.$(TARGET_BOARD_PLATFORM)
